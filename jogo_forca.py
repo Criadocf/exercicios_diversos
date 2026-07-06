cca4fc7dac7615db41d3363d584242f73a4950d3
kajota <kleberjrmmxvi@gmail.com>
2026-07-06 06:06:57 -0300

    jogo_da_forca

--- jogo_forca.py
+++ jogo_forca.py
@@ -1,3 +1,4 @@
+
 #ESQUELETO DO JOGO DA FORCA, TODAS AS LINHAS DE CÓDIGO SAIRÃO DESSA ESTRUTURA.
 #palavra = 'caju'
 
@@ -15,21 +16,28 @@
   ######
 
 
-palavra = 'FORMIGA'
+palavra = 'RIVALDO'
 
-letras_certas = ['_'] * (len(palavra))
+letras_certas = ['_ '] * (len(palavra))
 
 acertou = False
 
-credito = int(5)
+tentativas = 5
 
 while acertou == False:
+  letras_certas_copia = letras_certas.copy()
   chute = str(input('DIGITE UMA LETRA: ').upper())
-  for c in palavra:
-    if chute == c:
-      letras_certas[palavra.index(c)] = chute
-    else:
-      credito -= 1
-  print(''.join(letras_certas), palavra, credito)
+  for indice, letra in enumerate(palavra):
+    if chute == letra:
+      letras_certas[indice] = chute
+
+  if letras_certas == letras_certas_copia:
+      tentativas -= 1
+      if tentativas == 0:
+        print('Suas tentativas acabaram, GAME OVER FILHOTE')
+        break
+
+  print(''.join(letras_certas), tentativas) #junto as strings que foram 'jogadas soltas' dentro da lista, dessa forma imprimo como uma palavra completa.
   if palavra == ''.join(letras_certas):
+    print('PARABÉNS, VOCÊ ACERTOU')
     acertou = True
